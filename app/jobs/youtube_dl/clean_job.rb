25b58add51329234b0c45ba1dc8fdeba88141f75
Georges Stefan <stefangeorges4799@gmail.com>
2022-05-21 15:41:15 -0300

    Adds module YoutubeDl to jobs inside youtube_dl directory

--- app/jobs/youtube_dl/clean_job.rb
+++ app/jobs/youtube_dl/clean_job.rb
@@ -1,9 +1,11 @@
-class CleanJob < ApplicationJob
-  queue_as :default
+module YoutubeDl
+  class CleanJob < ApplicationJob
+    queue_as :default
 
-  def perform
-    p "Deleting mp3 files"
-    Dir.glob(File.join(Rails.root, "?.mp3")).each { |file| File.delete(file) }
-    p "mp3 files deleted"
+    def perform
+      p "Deleting mp3 files"
+      Dir.glob(File.join(Rails.root, "?.mp3")).each { |file| File.delete(file) }
+      p "mp3 files deleted"
+    end
   end
 end
--- app/jobs/youtube_dl/download_job.rb
+++ app/jobs/youtube_dl/download_job.rb
@@ -1,18 +1,20 @@
-class DownloadJob < ApplicationJob
-  queue_as :default
+module YoutubeDl
+  class DownloadJob < ApplicationJob
+    queue_as :default
 
-  def perform(filename, url)
-    command = "youtube-dl --newline --no-warnings --extract-audio --audio-format mp3 --output '#{filename}.mp3' #{url}" #| grep -e ETA -e \"Downloading video #\""
+    def perform(filename, url)
+      command = "youtube-dl --newline --no-warnings --extract-audio --audio-format mp3 --output '#{filename}.mp3' #{url}" #| grep -e ETA -e \"Downloading video #\""
 
-    Open3.popen3(command) do |stdin, stdout, stderr|
-      while (line = stdout.gets)
-        message = line.strip.split
-        if message.include? "[download]"
-          ActionCable.server.broadcast "youtube_dl_channel", message[1]
+      Open3.popen3(command) do |stdin, stdout, stderr|
+        while (line = stdout.gets)
+          message = line.strip.split
+          if message.include? "[download]"
+            ActionCable.server.broadcast "youtube_dl_channel", message[1]
+          end
         end
       end
-    end
 
-    ActionCable.server.broadcast "youtube_dl_channel", "[link] #{File.join(Rails.root, "#{filename}.mp3")}"
+      ActionCable.server.broadcast "youtube_dl_channel", "[link] #{File.join(Rails.root, "#{filename}.mp3")}"
+    end
   end
 end
