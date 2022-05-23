b8a74f85e6fbb38b34594c275c6c0d31e8f11182
Georges Stefan <stefangeorges4799@gmail.com>
2022-05-22 21:50:36 -0300

    Adds whenever gem and finishes clean job logic

--- app/jobs/youtube_dl/clean_job.rb
+++ app/jobs/youtube_dl/clean_job.rb
@@ -3,9 +3,19 @@ module YoutubeDl
     queue_as :default
 
     def perform
-      p "Deleting mp3 files"
-      Dir.glob(File.join(Rails.root, "?.mp3")).each { |file| File.delete(file) }
-      p "mp3 files deleted"
+      deleted_filenames = []
+
+      puts 'Deleting mp3 files'
+
+      Dir.glob(File.join(Rails.root, "*.mp3")).each do |file|
+        deleted_filenames << file.split('/')[-1].chomp('.mp3')
+        File.delete(file)
+      end
+
+      puts "Deleted #{deleted_filenames.count} mp3 files: "
+      deleted_filenames.each_with_index do |filename, index|
+        puts "  #{index+1}.- #{filename}"
+      end
     end
   end
 end
